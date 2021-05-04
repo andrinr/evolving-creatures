66dda536c08de7683c1ba3bf04bd6562973e5c37
Andrin Rehmann <andrinrehmann@gmail.com>
2021-05-04 10:24:20 +0200

    removed grid.py

--- grid.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-class grid:
-
-    def __init__(self, N, M):
-        self.rg = np.random.default_rng()
-        self.height = self.rg.random((N,M))
-        self.food = self.rg.random((N,M))
-
