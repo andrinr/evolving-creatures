17339d70b8a3de2d1e35a95947628b79aaa5d610
roschib <ronny.schibli@uzh.ch>
2021-05-24 15:07:07 +0200

    Create genome.py

---
+++ evolving-creatures-stats/genome.py
@@ -0,0 +1,61 @@
+from operator import attrgetter
+import numpy as np
+from scipy.stats import norm
+import math
+
+class Genome:
+    rg =  np.random.default_rng()
+
+    # {name: (min, max)}
+    bounds = {'speed':(1, 6),
+              'nKids': (1, 5),
+              'energyChildrenThreshold': (1, 5),
+              'toEnemies': (-3, 3),
+              'toFriends': (-3, 3),
+              'genomeThreshold': (0, 2)
+              }
+
+
+    def __init__(self, genes=None, idx=None, names=None):
+        if np.any(genes):
+            self.genes = genes 
+            self.idx = idx
+        else:
+            self.names = ['energyChildrenThreshold', 'nKids']
+            self.names += names
+            self.idx = {}
+            self.idx.update({gene: i for i, gene in enumerate(self.names)})
+            self.genes = self.randomGenes()
+
+
+    def randomGenes(self):
+        randFact = self.rg.random(len(self.names))
+        bounds = np.array([self.bounds[gene] for gene in self.names])
+        return bounds[:,0] + randFact * (bounds[:,1] - bounds[:,0])
+
+
+    def get(self, name):
+        return self.genes[self.idx[name]]
+
+
+    def replicate(self, drift):
+        bounds = np.array([self.bounds[gene] for gene in self.idx.keys()])
+
+        # adapt drift to the different gene-boundaries
+        drift *= (bounds[:,1] - bounds[:,0])
+
+        mutated = self.genes + np.array([self.rg.uniform(low=-drift[i]/2, high=drift[i]/2) for i in range(len(bounds))])
+
+        # ensure that the values are inside the boundaries
+        for gene in self.idx.keys():
+            lower, upper = bounds[self.idx[gene]]
+            if lower > mutated[self.idx[gene]]:
+                mutated[self.idx[gene]] = lower
+            elif upper < mutated[self.idx[gene]]:
+                mutated[self.idx[gene]] = upper
+
+        return Genome(genes=mutated, idx=self.idx)
+
+
+    def difference(self, other):
+        return np.linalg.norm(self.genes-other.genes)
